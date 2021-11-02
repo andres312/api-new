5315f7a45f1aeec974122031287629e244d1982d
Andres Rodriguez <andresroga@gmail.com>
2021-11-02 17:40:05 -0600

    first test storing information on post table add to service api/posts validating json structure,  return of json title and status 201 created, and database content ont table posts.
    Created migration with id, title and timestamps.
    
    Created controller and in store method creating all post fileds in request, and return status 201.
    
    Created resource and route for resource.

---
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostControllerTest extends TestCase
+{
+    use RefreshDatabase;
+    public function test_store()
+    {
+        //$this->withoutExceptionHandling();
+        $response = $this->json('POST', '/api/posts', [
+            'title' => 'El post de prueba'
+        ]);
+
+        $response->assertJsonStructure(['id', 'title', 'created_at', 'updated_at'])//validation of json fields
+        ->assertJson(['title' => 'El post de prueba'])// what information is in title
+        ->assertStatus(201) //OK, resource created
+        ;
+
+        $this->assertDatabaseHas('posts', ['title' => 'El post de prueba']);//table post has that title
+    }
+}
